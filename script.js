47e8bae075a1089fe970dec23d226494d73abd58
microtoolabs <agdbat10@gmail.com>
2026-08-11 14:45:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const basicForm = document.querySelector("form");
-
 const scopeSection = document.querySelector("#scopeSection");
+const backButton = document.querySelector("#backButton");
 
 basicForm.addEventListener("submit", function(event) {
     event.preventDefault();
 
     const customer = document.querySelector("#customer").value;
 
-    const message = document.querySelector("#message");
-
-    message.textContent = "Cliente: " + customer;
-
     basicForm.style.display = "none";
     scopeSection.style.display = "block";
+});
+
+backButton.addEventListener("click", function() {
+    scopeSection.style.display = "none";
+    basicForm.style.display = "block";
 });
