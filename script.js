ee4e0266e692aea3548dd117c343da5fa06bd519
microtoolabs <agdbat10@gmail.com>
2026-08-11 15:22:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const basicForm = document.querySelector("form");
 const scopeSection = document.querySelector("#scopeSection");
 const backButton = document.querySelector("#backButton");
+const includedItems = document.querySelector("#includedItems");
+const addIncluded = document.querySelector("#addIncluded");
 
 basicForm.addEventListener("submit", function(event) {
     event.preventDefault();
@@ -12,4 +14,15 @@ basicForm.addEventListener("submit", function(event) {
 backButton.addEventListener("click", function() {
     scopeSection.style.display = "none";
     basicForm.style.display = "block";
+});
+
+addIncluded.addEventListener("click", function() {
+
+    const newItem = document.createElement("input");
+
+    newItem.type = "text";
+    newItem.placeholder = "Describe an included item";
+
+    includedItems.appendChild(newItem);
+
 });
